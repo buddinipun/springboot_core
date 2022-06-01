de5c0d40d56a7b2571b745bd831af037f762ad3b
buddinipun <buddi@Buddhikas-MacBook-Air.local>
2022-06-01 13:56:44 +0530

    xml configurations for base project

--- src/main/java/buddi/springfamework/controllers/GreetingController.java
+++ src/main/java/buddi/springfamework/controllers/GreetingController.java
@@ -3,33 +3,47 @@ package buddi.springfamework.controllers;
 import buddi.springfamework.services.HelloWorldService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
 
-@Controller
+//@Controller
 public class GreetingController {
 
     private HelloWorldService helloWorldService;
+    private HelloWorldService helloWorldServiceSpanish;
+    private HelloWorldService helloWorldServiceGerman;
+    private HelloWorldService helloWorldServiceEnglish;
+
 
     public void setHelloWorldService(HelloWorldService helloWorldService) {
         this.helloWorldService = helloWorldService;
     }
 
-    @Autowired
-    @Qualifier("HelloWorldServiceGerman")
+
+
     public void setHellowWolrdServiceGerman(HelloWorldService helloWorldServiceGerman) {
-        this.helloWorldService = helloWorldServiceGerman;
+        this.helloWorldServiceGerman = helloWorldServiceGerman;
     }
-    @Autowired
-    @Qualifier("spanish")
+
+
+    public void setHelloWorldServiceEnglish(HelloWorldService helloWorldServiceEnglish) {
+        this.helloWorldServiceEnglish = helloWorldServiceEnglish;
+    }
+
+
     public void setHelloWorldServiceSpanish(HelloWorldService helloWorldServiceSpanish) {
-        this.helloWorldService = helloWorldServiceSpanish;
+        this.helloWorldServiceSpanish = helloWorldServiceSpanish;
     }
     public String sayHello() {
 
 
         String greeting = helloWorldService.getGreeting();
-        System.out.println( "testing"+ greeting);
+        System.out.println( greeting);
+        System.out.println(helloWorldServiceGerman.getGreeting());
+      //  System.out.println(helloWorldServiceSpanish.getGreeting());
         return greeting;
+
 
 
     }
--- src/main/java/buddi/springfamework/springboot/DependancyInjectionApplication.java
+++ src/main/java/buddi/springfamework/springboot/DependancyInjectionApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ComponentScan("buddi.springfamework")
+//@ComponentScan("buddi.springfamework")
+@ImportResource("classpath:/spring/spring-config.xml")
 public class DependancyInjectionApplication {
 
     public static void main(String[] args) {
